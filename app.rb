7fa43aeb25fe208f5046502171990bb013d9ee14
gerrysapienza <ggsap1@gmail.com>
2020-05-21 02:26:35 +0000

    Iteration 1

--- app.rb
+++ app.rb
@@ -20,3 +20,36 @@ get "/" do
   puts @events.inspect
   view "events"
 end
+
+# the :id gives you a dynammic input to the URL where : gives dynamic and id identifies the hash
+get "/events/:id" do
+    # SELECT * FROM events WHERE id=:id
+    @event = events_table.where(:id => params["id"]).to_a[0]
+    # SELECT * FROM rsvps WHERE event_id=:id
+    @rsvps = rsvps_table.where(:event_id => params["id"]).to_a
+    # SELECT COUNT(*) FROM rsvps WHERE event_id=:id AND going=1
+    @count = rsvps_table.where(:event_id => params["id"], :going => true).count
+    puts @event.inspect
+    puts params.inspect
+    view "event"
+end
+
+#code only runs on this URL so you must redefine a new @event
+get "/events/:id/rsvps/new" do
+    @event = events_table.where(:id => params["id"]).to_a[0]
+    view "new_rsvp"
+end
+get "/events/:id/rsvps/create" do
+    #still need to do stuff here to create the RSVP in the DB
+    puts params.inspect
+    rsvps_table.insert(:event_id => params["id"],
+                        :going => params["going"],
+                        :name => params["name"],
+                        :email => params["email"],
+                        :comments => params["comments"])
+    view "create_rsvp"
+end
+#creates a URL
+#get "/tacos" do
+#    view "tacos"
+#end
